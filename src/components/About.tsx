38475456613d627adc2a44df939fe83fa2e58524
grignaffini <grignaffinipaolo@gmail.com>
2023-02-20 16:40:06 +0100

    updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,31 +1,52 @@
-import { motion } from 'framer-motion'
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
 
 function About() {
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className='flex flex-col text-center md:text-left relative h-screen md:max-w-7xl px-10 justify-evenly mx-auto items-center'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
-            <motion.img
-                initial={{ x: -200, opacity: 0 }}
-                transition={{ duration: 1 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                className="-mb-20 mt-16 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[300px] xl:h-[400px]"
-                src="/me.jpg" />
-            <div className='space-y-10 mx-0 md:px-10'>
-                <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7AB0A]'>little</span> background</h4>
-                <p className='text-base'>
-                    Reliable and passionate individual looking for a position within a blockchain development environment.
-                    Throughout my education, I have demonstrated high levels of motivation, work ethic and teamwork, gaining valuable knowledge on both computer science theory and practice. During the past couple of years I have worked as a DevOps Engineer for an international bank. During my time spent there I started approaching blockchain technology in order to get a better understanding of what could constitute an alternative to the traditional financial system.
-                    During the last months I took part on numerous Web3 hackathons: ETHGlobal, Polakadot N.A., Encode x Algorand... where I got excellent results implementing innovative ideas and presenting them to the Web3-community.
-                    I am currently seeking a job that could offer me the opportunity to build the next killer application on Web3.
-                </p>
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative mx-auto flex h-screen flex-col items-center justify-evenly px-10 text-center md:max-w-7xl md:text-left"
+    >
+      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
+        About
+      </h3>
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        className="-mb-20 mt-16 h-56 w-56 flex-shrink-0 rounded-full object-cover md:mb-0 md:h-96 md:w-64 md:rounded-lg xl:h-[400px] xl:w-[300px]"
+        src="/me.jpg"
+      />
+      <div className="mx-0 space-y-10 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]">little</span>{" "}
+          background
+        </h4>
+        <p className="text-base">
+          Hey there! 👋 <br /> I&apos;m a reliable and passionate individual
+          who&apos;s eager to contribute my skills to a blockchain development
+          team👨🏻‍💻 <br />
+          Through my education and work experience as a DevOps Engineer at an
+          international bank, I&apos;ve gained extensive knowledge and hands-on
+          experience in computer science 🤓 <br />
+          Recently, I&apos;ve been diving deep into the world of blockchain
+          technology, participating in several Web3 hackathons such as
+          ETHGlobal, Polkadot N.A., and Encode x Algorand. It&apos;s been a
+          blast implementing innovative ideas and presenting them to the vibrant
+          Web3-community 🚀 <br />
+          I&apos;m currently on the lookout for a job that would give me the
+          opportunity to work on building the next big thing on Web3 🛠 <br />
+          Also, interesting fact about me: I lived in New Mexico for two years
+          while pursuing my master&apos;s degree. It was an incredible
+          experience and I&apos;m grateful for the knowledge and skills I gained
+          during my time there 🇺🇸
+        </p>
+      </div>
+    </motion.div>
+  );
 }
 
-export default About
+export default About;
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,58 +1,74 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import ExperienceCard from './ExperienceCard'
+import React from "react";
+import { motion } from "framer-motion";
+import ExperienceCard from "./ExperienceCard";
 
 function WorkExperience() {
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className='h-screen flex relative overflow-hidden flex-col text-left 
-            md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-                Experience
-            </h3>
-            <div className='w-full flex space-x-5 overflow-x-scroll p-10 
-            snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 
-            scrollbar-thumb-[#F7Ab0A]/80'>
-                <ExperienceCard
-                    image="/nmsu.png"
-                    society='New Mexico State University'
-                    role='Web Administrator'
-                    startDate='1/2018'
-                    endDate='5/2019'
-                    summary={["Web administrator for the college of Arts&Sciences"]}
-                    skills={["/html.png", "/css.png", "/js.png"]} />
-                <ExperienceCard
-                    image="/ca.png"
-                    society='Credit Agricole Group Solutions'
-                    role='DevOps Engineer'
-                    startDate='2/2020'
-                    endDate='3/2022'
-                    summary={[
-                        "Architectural documentation of the company's infrastructure",
-                        "Automation of CI/CD pipelines",
-                        "Implementation of infrastructure provisioning, monitoring and logging",
-                        "Project management with Azure DevOps"
-                    ]}
-                    skills={["python.png", "jenkins.png", "azure.jpeg"]} />
-                <ExperienceCard
-                    image="/web3.jpeg"
-                    society=''
-                    role='Freelance Web3 Developer'
-                    startDate='3/2022'
-                    summary={[
-                        "Developed, implemented and deployed Web3 full-stack applications",
-                        "Greengo: decentralized crowdfunding made on Algorand",
-                        "PostThread: decentralized social media made on Polkadot",
-                        "BazaarMarket: decentralized nft trading marketplace made on Polygon",
-                        "For further details see CV"
-                    ]}
-                    skills={["react.png", "next.png", "tailwind.png", "graphql.png", "trpc.svg", "postgres.png", "js.png", "ts.png"]} />
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative mx-auto flex h-screen max-w-full flex-col 
+            items-center justify-evenly overflow-hidden px-10 text-left md:flex-row"
+    >
+      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
+        Experience
+      </h3>
+      <div
+        className="flex w-full snap-x snap-mandatory space-x-5 
+            overflow-x-scroll p-10 scrollbar-thin scrollbar-track-gray-400/20 
+            scrollbar-thumb-[#F7Ab0A]/80"
+      >
+        <ExperienceCard
+          image="/nmsu.png"
+          society="New Mexico State University"
+          role="Web Administrator"
+          startDate="1/2018"
+          endDate="5/2019"
+          summary={["Web administrator for the college of Arts&Sciences"]}
+          skills={["/html.png", "/css.png", "/js.png"]}
+        />
+        <ExperienceCard
+          image="/ca.png"
+          society="Credit Agricole Group Solutions"
+          role="DevOps Engineer"
+          startDate="2/2020"
+          endDate="3/2022"
+          summary={[
+            "Architectural documentation of the company's infrastructure",
+            "Automation of CI/CD pipelines",
+            "Implementation of infrastructure provisioning, monitoring and logging",
+            "Project management with Azure DevOps",
+          ]}
+          skills={["python.png", "jenkins.png", "azure.jpeg"]}
+        />
+        <ExperienceCard
+          image="/web3.jpeg"
+          society=""
+          role="Freelance Web3 Developer"
+          startDate="3/2022"
+          summary={[
+            "Developed, implemented and deployed Web3 full-stack applications",
+            "Greengo: decentralized crowdfunding made on Algorand",
+            "PostThread: decentralized social media made on Polkadot",
+            "BazaarMarket: decentralized nft trading marketplace made on Polygon",
+            "UnstableLabs: Ai-generated NFTs made on Polygon",
+            "For further details see CV",
+          ]}
+          skills={[
+            "react.png",
+            "next.png",
+            "tailwind.png",
+            "graphql.png",
+            "trpc.svg",
+            "postgres.png",
+            "js.png",
+            "ts.png",
+          ]}
+        />
+      </div>
+    </motion.div>
+  );
 }
 
-export default WorkExperience
+export default WorkExperience;
